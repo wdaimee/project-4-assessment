c2aeb1875ea4f22aa6eaf2474b4c283ac31d51a8
Waqas D <wdaimee@gmail.com>
2020-05-08 16:14:22 -0400

    Updates

---
+++ precache-manifest.c0d2e1f81e7d6f4a1af1bf47e37336eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34830edf3d42afc0d6d27167610879dc",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "de302367c36d5520c5bd",
+    "url": "/project-4-assessment/static/css/main.2ddb3bd6.chunk.css"
+  },
+  {
+    "revision": "2c2f3676c6882e8399ef",
+    "url": "/project-4-assessment/static/js/2.c08036ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.c08036ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de302367c36d5520c5bd",
+    "url": "/project-4-assessment/static/js/main.e7dd16c0.chunk.js"
+  },
+  {
+    "revision": "364c0338e2360efff690",
+    "url": "/project-4-assessment/static/js/runtime-main.6ff2799a.js"
+  }
+]);
